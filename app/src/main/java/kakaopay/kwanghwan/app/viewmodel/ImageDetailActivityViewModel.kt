e20352cea6d86fffe8190177d2bf3d4452d8ed7e
kwanghwan <kwanghwan@starshell.co>
2018-07-24 23:56:28 +0900

    [공유하기] 기본 공유하기 추가

--- app/src/main/java/kakaopay/kwanghwan/app/viewmodel/ImageDetailActivityViewModel.kt
+++ app/src/main/java/kakaopay/kwanghwan/app/viewmodel/ImageDetailActivityViewModel.kt
@@ -1,14 +1,27 @@
 package kakaopay.kwanghwan.app.viewmodel
 
+import android.content.Intent
 import android.databinding.BaseObservable
 import android.view.View
 import kakaopay.kwanghwan.app.model.KakaoImage
 import kakaopay.kwanghwan.app.ui.activity.ImageDetailActivity
 import kakaopay.kwanghwan.app.util.CommonUtil
 
+
+
+
+
 class ImageDetailActivityViewModel(var activity: ImageDetailActivity, var kakaoImage: KakaoImage) : BaseObservable() {
     fun back(view: View) {
         activity.onBackPressed()
+    }
+
+    fun share(view: View) {
+        val intent = Intent(android.content.Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "이미지 공유합니다.\n${kakaoImage.imageUrl}")
+        val chooser = Intent.createChooser(intent, "친구에게 공유하기")
+        activity.startActivity(chooser)
     }
 
     val datetime: String
