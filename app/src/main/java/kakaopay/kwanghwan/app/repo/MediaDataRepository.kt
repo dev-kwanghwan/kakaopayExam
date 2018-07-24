f530de03d714be2fb631d016d23d52f245dee32d
kwanghwan <kwanghwan@starshell.co>
2018-07-25 01:30:14 +0900

    [검색] 정확도순, 시간순 정렬

--- app/src/main/java/kakaopay/kwanghwan/app/repo/MediaDataRepository.kt
+++ app/src/main/java/kakaopay/kwanghwan/app/repo/MediaDataRepository.kt
@@ -3,6 +3,8 @@ package kakaopay.kwanghwan.app.repo
 import android.content.Context
 import kakaopay.kwanghwan.app.interfaces.MediaService
 import kakaopay.kwanghwan.app.model.KakaoMedia
+import kakaopay.kwanghwan.app.util.CommonUtil
+import java.util.*
 
 /**
  * KakaoMedia(동영상, 이미지) 데이터를 관리합니다.
@@ -20,6 +22,14 @@ class MediaDataRepository(private val context: Context) : MediaService {
      * 데이터 정렬
      */
     private fun sort(origin: ArrayList<KakaoMedia>): ArrayList<KakaoMedia> {
+        val comparator = { k1: KakaoMedia, k2: KakaoMedia ->
+            val k1Millisecond = CommonUtil.timeConvertToMillisecond(context, k1.date)
+            val k2Millisecond = CommonUtil.timeConvertToMillisecond(context, k2.date)
+            java.lang.Long.compare(k1Millisecond!!, k2Millisecond!!)
+        }
+
+        Collections.sort(origin, comparator)
+        origin.reverse()
         return origin
     }
 }
